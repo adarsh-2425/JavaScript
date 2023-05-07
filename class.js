c518fb25a40580587e71d32d0f7d131267a9b72e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-07 10:26:28 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,14 @@
+//Class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let person = new Person('Alice', 14);
+console.log(person.age);
+
+//class is a keyword in JavaScript that is used to define a new class. It was introduced in ECMAScript 2015 (also known as ES6) and provides a new syntax for creating classes and defining their methods.
+
+//The class keyword allows us to define an object-oriented class with a constructor method and other methods.
