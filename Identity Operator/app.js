69f041db735d476a383b266911f7b4c1f0e5161a
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-06 11:26:12 +0530

    Create app.js

---
+++ Identity Operator/app.js
@@ -0,0 +1,19 @@
+//equality operator
+let a = 10;
+let b = '10';
+if (a == b) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+//output is true
+
+//identity operator
+let a = 10;
+let b = '10';
+if (a === b) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+//output is false
