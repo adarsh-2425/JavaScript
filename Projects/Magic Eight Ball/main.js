fc9cfa46c4d586cdfbf7562b3b5d3fd31608ac37
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-07 08:23:18 +0530

    Create main.js

---
+++ Projects/Magic Eight Ball/main.js
@@ -0,0 +1,43 @@
+//Magic Eight Ball
+const userName = '';
+  userName?
+    console.log(`Hello ${userName}`) :
+    console.log('Hello');
+
+const userQuestion = '';
+console.log(userQuestion);
+
+//random number between 0 and 7
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall  = '';
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+   eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    console.log('ERROR! Try Again.');
+}
+
+console.log(eightBall);
