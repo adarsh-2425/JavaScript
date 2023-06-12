de27b90ec5546b78bfcfd3c05db77b9975ae0df8
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-12 08:38:42 +0530

    Create forEach() Method.js

---
+++ Iteration Methods/forEach() Method.js
@@ -0,0 +1,12 @@
+//Question : Iterate over the fruits array to log 'I want to eat a' plus the name of each fruit to the console. For example, 'I want to eat a mango'.
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+// Output:
+// I want to eat a mango
+// I want to eat a papaya
+// I want to eat a pineapple
+// I want to eat a apple
