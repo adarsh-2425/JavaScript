53bc574d549c1c47e0ea1d3b4f946d7409cb09c4
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-14 12:10:37 +0530

    Create array of objects.js

---
+++ Objects/Nested Objects/array of objects.js
@@ -0,0 +1,8 @@
+const myArray = [
+  { name: 'Alice', age: 30 },
+  { name: 'Bob', age: 25 },
+  { name: 'Charlie', age: 35 }
+];
+
+const alice = myArray[0];
+console.log(alice.name); // this will log "Alice"
