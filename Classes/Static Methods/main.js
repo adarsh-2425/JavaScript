5ed0fe379022426b463743f136c8abecbf58570e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-17 17:48:33 +0530

    Create main.js

---
+++ Classes/Static Methods/main.js
@@ -0,0 +1,17 @@
+class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+ 
+  static generateName() {
+    const names = ['Angel', 'Spike', 'Buffy', 'Willow', 'Tara'];
+    const randomNumber = Math.floor(Math.random()*5);
+    return names[randomNumber];
+  }
+} 
+
+console.log(Animal.generateName()); // returns a name
+
+const tyson = new Animal('Tyson'); 
+tyson.generateName(); // TypeError
