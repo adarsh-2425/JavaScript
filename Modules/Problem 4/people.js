69ef5338f2cd6a0e039080bb0d98a02c22e332bc
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-19 19:19:55 +0530

    Create people.js

---
+++ Modules/Problem 4/people.js
@@ -0,0 +1,9 @@
+const peoples = [
+  { name: 'Alice', age: 25, gender: 'female' },
+  { name: 'Bob', age: 17, gender: 'male' },
+  { name: 'Charlie', age: 30, gender: 'male' },
+  { name: 'Dave', age: 16, gender: 'male' },
+  { name: 'Eve', age: 22, gender: 'female' }
+];
+
+module.exports = { peoples };
