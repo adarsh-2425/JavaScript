a435e3b4c16a40addfc5a1fca0f668f1268aa82e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-20 18:36:48 +0530

    Create chaining.js

---
+++ Promises/chaining.js
@@ -0,0 +1,9 @@
+//This process of chaining promises together is called composition. Promises are designed with composition in mind!
+
+firstPromiseFunction()
+.then((firstResolveVal) => {
+  return secondPromiseFunction(firstResolveVal);
+})
+.then((secondResolveVal) => {
+  console.log(secondResolveVal);
+});
