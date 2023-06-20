e18bbe391d55f33e9220702371fe470a75ef622f
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-20 17:24:18 +0530

    Create catch.js

---
+++ Promises/catch.js
@@ -0,0 +1,20 @@
+/*The .catch() function takes only one argument, onRejected. In the case of a rejected promise, 
+this failure handler will be invoked with the reason for rejection. 
+Using .catch() accomplishes the same thing as using a .then() with only a failure handler.*/
+
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+// Write your code below:
+checkInventory(order)
+  .then(handleSuccess)
+  .catch(handleFailure)
