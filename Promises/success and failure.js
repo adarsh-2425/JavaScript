2ae94092b87cba8924e93f81cb264d42b3ff9ce3
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-20 16:31:17 +0530

    Create success and failure.js

---
+++ Promises/success and failure.js
@@ -0,0 +1,20 @@
+//To handle a “successful” promise, or a promise that resolved, we invoke .then() on the promise, passing in a success handler callback function:
+
+let prom = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < .5 ){
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+ 
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+ 
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+ 
+prom.then(handleSuccess, handleFailure);
