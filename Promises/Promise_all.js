ae147b1090e152287b20f541c268905f1ebafd1e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-21 04:28:48 +0530

    Create Promise_all.js

---
+++ Promises/Promise_all.js
@@ -0,0 +1,12 @@
+//Promise.all()
+//Promise.all() accepts an array of promises as its argument and returns a single promise.
+
+let myPromises = Promise.all([returnsPromOne(), returnsPromTwo(), returnsPromThree()]);
+ 
+myPromises
+  .then((arrayOfValues) => {
+    console.log(arrayOfValues);
+  })
+  .catch((rejectionReason) => {
+    console.log(rejectionReason);
+  });
