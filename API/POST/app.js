6652f26054fb93ef8e5c6216529df71d991a31d8
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-26 17:30:56 +0530

    Update app.js

--- API/POST/app.js
+++ API/POST/app.js
@@ -1,6 +1,7 @@
 const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts';
 
 //1. Write a POST request that creates a new post with userId=5, title="My First Post", and body="This is my first post on JSONPlaceholder".
+
 fetch(API_ENDPOINT, {
   method: 'POST',
   body: JSON.stringify({
@@ -21,6 +22,7 @@ fetch(API_ENDPOINT, {
 
 /*2. Write a POST request that creates a new post with userId=3, title="New Blog Idea", 
 and body="I'm thinking about starting a new blog. What do you think I should write about?"*/
+
 fetch(API_ENDPOINT, {
   method: 'POST',
   body: JSON.stringify({
@@ -35,5 +37,25 @@ fetch(API_ENDPOINT, {
 .then(response => response.json())
 .then(data => {
   console.log('New Post Created: ', data);
+})
+.catch(error => console.log(error));
+
+/*3. Write a POST request that creates a new post with userId=2, title="My Travel Adventure", 
+and body="I just got back from a trip to Europe and it was amazing! Here are some of my favorite moments."*/
+
+fetch(API_ENDPOINT, {
+  method: 'POST',
+  body: JSON.stringify({
+    userId: 2,
+    title: 'My Travel Adventure',
+    body: "I just got back from a trip to Europe and it was amazing! Here are some of my favorite moments."
+  }),
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8'
+  },
+})
+.then(response => response.json())
+.then(data => {
+  console.log('New post created:', data)
 })
 .catch(error => console.log(error));
