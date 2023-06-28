2d1f51d9e438e51fdcda69663fa9cd1b86763437
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-06-28 09:16:07 +0530

    Create basic problem.js

---
+++ Async...Await/basic problem.js
@@ -0,0 +1,13 @@
+// Creating a new promise that runs the function in the setTimeout after 5 seconds. 
+const newPromise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('done'), 5000)
+})
+// Creating an asynchronous function using an arrow expression and saving it to a the variable asyncFunction. 
+const asyncFunction = async() => {
+  // Awaiting the promise to resolve and saving the result to the variable result.
+  const result = await newPromise;
+  // Logging the result of the promise to the console
+  console.log(result); // Output: done
+}
+
+asyncFunction()
