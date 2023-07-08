98c2ac22f525720935dfc8032271f298baa9c08e
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-08 12:00:50 +0530

    Create Problem 1.js

---
+++ Slice Method/Problem 1.js
@@ -0,0 +1,9 @@
+//Write a function that takes an array of numbers as input and returns a new array with the first three elements.
+
+function lastThree(nums) {
+  const result =  nums.slice(0, 3);
+  console.log(result);
+}
+
+const nums = [1, 2, 3, 4, 5];
+lastThree(nums);
