ee934e1cff637eb0c5f1d32deeeb020b5537a551
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-07-27 17:35:38 +0530

    Create Problem1.js

---
+++ Set Objects/Problem1.js
@@ -0,0 +1,7 @@
+/*Let's say you have an array of numbers, and you want to find all the unique numbers in the array. You could use a Set object to accomplish this task,*/
+
+const numbers = [1, 1, 1, 1, 1, 2, 3];
+
+const uniqueNumbers = new Set(numbers);
+
+console.log(uniqueNumbers)
