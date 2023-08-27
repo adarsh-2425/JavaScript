d5eabc02e1dfb685134b4f0babd90c5d7163cb60
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-08-27 14:49:57 +0530

    Update helper function1.js

--- Problems/Intermediate/helper function1.js
+++ Problems/Intermediate/helper function1.js
@@ -2,8 +2,14 @@
 If the array is empty or doesn't contain any even numbers, the function should return 0.*/
 
 function findEvenSum(nums) {
-    return nums.filter(num => num % 2 === 0)
-                .reduce((a, b) => a + b, 0)
+    // checking for even number
+    const evenNumbers = nums.filter(num => num % 2 === 0);
+    
+    if (evenNumbers.length === 0) {
+        return 0;
+    }
+    
+    return evenNumbers.reduce((a, b) => a + b, 0);
 }
 
 const nums = [1, 2, 3, 4, 5, 6];
